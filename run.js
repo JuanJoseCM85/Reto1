7ac7a8103d8a41d30f6194af352d01e57333c849
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-01-17 12:26:37 +0100

    Primera commit reto 1

---
+++ run.js
@@ -0,0 +1,9 @@
+let myLib = require("./index");
+let a = 2;
+let b = 3;
+
+console.log(myLib.suma(a,b));
+console.log(myLib.resta(a,b));
+console.log(myLib.producto(a,b));
+console.log(myLib.division(a,b));
+console.log(myLib.cuadrado(a));
